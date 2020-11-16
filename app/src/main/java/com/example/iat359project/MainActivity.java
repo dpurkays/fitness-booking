f4b6f6bd85e14712d7a01c98f185429a2aee0595
VinceH <vhoang@sfu.ca>
2020-11-16 15:18:23 -0800

    AccelerometerActivity added + SignInActivity + SignUpActivity

--- app/src/main/java/com/example/iat359project/MainActivity.java
+++ app/src/main/java/com/example/iat359project/MainActivity.java
@@ -17,16 +17,36 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button accelerometerButton, signInButton, signUpButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button accelerometerButton = (Button) findViewById(R.id.accelerometerButton);
+        accelerometerButton = (Button) findViewById(R.id.accelerometerButton);
         accelerometerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startAccelerometerActivity();
+            }
+        });
+
+        signInButton = (Button) findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startSignInActivity();
+            }
+        });
+
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startSignUpActivity();
             }
         });
     }
@@ -40,6 +60,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void startAccelerometerActivity(){
         Intent i = new Intent(this.getApplicationContext(),AccelerometerActivity.class);
+        startActivity(i);
+    }
+
+    public void startSignInActivity(){
+        Intent i = new Intent(this.getApplicationContext(),SignInActivity.class);
+        startActivity(i);
+    }
+
+    public void startSignUpActivity(){
+        Intent i = new Intent(this.getApplicationContext(),SignUpActivity.class);
         startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/iat359project/SignUpActivity.java
@@ -0,0 +1,21 @@
+package com.example.iat359project;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+    Spinner daySpinner, daytimeSpinner, monthSpinner, hourSpinner
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        daySpinner = (Spinner) findViewById(R.id.daySpinner);
+        daytimeSpinner = (Spinner) findViewById(R.id.daytimeSpinner);
+        monthSpinner = (Spinner) findViewById(R.id.monthSpinner);
+        hourSpinner = (Spinner) findViewById(R.id.hourSpinner);
+
+    }
+}
